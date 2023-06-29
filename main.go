bec4757a8bfd9c93590129e61697972f2820434b
DevMiner <devminer@devminer.xyz>
2023-06-29 09:13:31 +0200

    impl: JSON RPC endpoint

--- main.go
+++ main.go
@@ -10,6 +10,16 @@ import (
 	"github.com/netresearch/ldap-selfservice-password-changer/internal/web/templates"
 )
 
+type JSONRPC struct {
+	Method string   `json:"method"`
+	Params []string `json:"params"`
+}
+
+type JSONRPCResponse struct {
+	Success bool     `json:"success"`
+	Data    []string `json:"data"`
+}
+
 func main() {
 	views := html.NewFileSystem(http.FS(templates.Templates), ".html")
 
@@ -23,6 +33,21 @@ func main() {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("index", fiber.Map{})
+	})
+
+	app.Post("/api/rpc", func(c *fiber.Ctx) error {
+		var body JSONRPC
+		if err := c.BodyParser(&body); err != nil {
+			return err
+		}
+
+		switch body.Method {
+		default:
+			return c.Status(http.StatusNotFound).JSON(JSONRPCResponse{
+				Success: false,
+				Data:    []string{"method not found"},
+			})
+		}
 	})
 
 	app.Listen(":3000")
