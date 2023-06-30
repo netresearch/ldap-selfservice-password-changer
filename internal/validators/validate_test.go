361aaf0d9a3256211fc8e12641a87bae2e73698e
DevMiner <devminer@devminer.xyz>
2023-06-30 18:25:03 +0200

    test: add Go tests and CI

---
+++ internal/validators/validate_test.go
@@ -0,0 +1,106 @@
+package validators_test
+
+import (
+	"testing"
+
+	"github.com/netresearch/ldap-selfservice-password-changer/internal/validators"
+)
+
+type TestCase struct {
+	Input    string
+	Arg      int
+	Expected bool
+}
+
+func TestMinNumbersInString(t *testing.T) {
+	cases := []TestCase{
+		{
+			Input:    "asdf1f",
+			Arg:      1,
+			Expected: true,
+		},
+		{
+			Input:    "asdf1f",
+			Arg:      2,
+			Expected: false,
+		},
+	}
+
+	for _, c := range cases {
+		actual := validators.MinNumbersInString(c.Input, c.Arg)
+		if actual != c.Expected {
+			t.Errorf("expected %t, got %t", c.Expected, actual)
+		}
+	}
+}
+
+func TestMinSymbolsInString(t *testing.T) {
+	cases := []TestCase{
+		{
+			Input:    "asdf!f",
+			Arg:      1,
+			Expected: true,
+		},
+		{
+			Input:    "asdf!f",
+			Arg:      2,
+			Expected: false,
+		},
+		{
+			Input:    "a!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~b",
+			Arg:      32,
+			Expected: true,
+		},
+	}
+
+	for _, c := range cases {
+		actual := validators.MinSymbolsInString(c.Input, c.Arg)
+		if actual != c.Expected {
+			t.Errorf("expected %t, got %t", c.Expected, actual)
+		}
+	}
+}
+
+func TestMinUppercaseLettersInString(t *testing.T) {
+	cases := []TestCase{
+		{
+			Input:    "asdfF",
+			Arg:      1,
+			Expected: true,
+		},
+		{
+			Input:    "asdfF",
+			Arg:      2,
+			Expected: false,
+		},
+	}
+
+	for _, c := range cases {
+		actual := validators.MinUppercaseLettersInString(c.Input, c.Arg)
+		if actual != c.Expected {
+			t.Errorf("expected %t, got %t", c.Expected, actual)
+		}
+	}
+}
+
+func TestMinLowercaseLettersInString(t *testing.T) {
+	cases := []TestCase{
+		{
+			Input:    "asdfF",
+			Arg:      1,
+			Expected: true,
+		},
+		{
+			Input:    "asdfF",
+			Arg:      5,
+			Expected: false,
+		},
+	}
+
+	for _, c := range cases {
+		actual := validators.MinLowercaseLettersInString(c.Input, c.Arg)
+		if actual != c.Expected {
+			t.Errorf("expected %t, got %t", c.Expected, actual)
+		}
+	}
+}
